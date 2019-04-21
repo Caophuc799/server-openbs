cee33d331d8be3dfbfd38c4c538bc9c315cb5ab3
Cao Cao <caophuc799@gmail.com>
2019-04-21 16:14:40 +0700

    fix bug

--- services/Firebase.js
+++ services/Firebase.js
@@ -2,14 +2,7 @@ global.XMLHttpRequest = require('xhr2') //workaround for firebase request
 
 import firebase from 'firebase' // 4.8.1
 import 'firebase/storage'
-import * as admin from 'firebase-admin'
 
-var serviceAccount = require('./serviceAccountKey.json')
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://openbs-chat.firebaseio.com'
-})
 firebase.initializeApp({
   apiKey: 'AIzaSyDJ0Wmlyv6f1_v0vMQ-1wHE3qn_nK43RwA',
   authDomain: 'openbs-chat.firebaseapp.com',
